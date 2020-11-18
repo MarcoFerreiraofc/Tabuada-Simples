c4f9e6acc60ce40a024b3b6c92d195090b693528
Marco Ferreira <marcoantonio.fa@outlook.com>
2020-11-18 10:36:49 -0300

    Tabuada finalizada

--- Program.cs
+++ Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
-namespace Tabuada_simples
+namespace Backend_MultiplicationTable
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+            string refazer = "sim";
+            while(refazer == "sim"){
+
+                
+                Console.WriteLine("-------------------------------------------------");
+           
+                Console.WriteLine("Eae,eu sou sua tabua em forma de C# =)");
+               
+                Console.WriteLine("-------------------------------------------------");
+                System.Threading.Thread.Sleep(700);
+
+                Console.Write("Qual tabuada você quer ver?");
+           
+                Console.WriteLine(" (apenas números)");
+               
+                int tabuada = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+             
+                System.Threading.Thread.Sleep(700);
+                Console.WriteLine($"A tabuada de {tabuada} é:");
+
+                int resultado = 0;
+            
+                for(int contador = 0; contador <= 10; contador++){
+                    resultado = tabuada * contador;
+                  
+                    Console.Write($"{tabuada} x {contador} = ");
+                
+                    Console.WriteLine(resultado);
+
+                    System.Threading.Thread.Sleep(850);
+                }
+
+                Console.WriteLine("");
+              
+                Console.WriteLine("Ta afim de fazer outra a operação?");
+                System.Threading.Thread.Sleep(500);
+         
+                refazer = Console.ReadLine();
+
+                if(refazer != "sim"){
+                    System.Threading.Thread.Sleep(650);
+                    Console.WriteLine("");
+         
+                    Console.WriteLine("Valeu a preferência =)");
+                }
+                
+            }
         }
     }
 }
